7345da3485fc234b22cf46eb0209623e8fb874fe
Luiz Pedro Modenez <lmodenez98@gmail.com>
2025-05-14 00:34:54 -0300

    refactor: load environment variables only in development using config.py

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "main.py"]
+CMD ["sh", "-c", "echo 🔍 ENV VARS && env && python main.py"]
--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,25 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+if not os.getenv("RAILWAY_ENVIRONMENT"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+def str_list(value):
+    return [v.strip() for v in value.split(",") if v.strip()]
 
 class Settings:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    CHAT_ID = os.getenv("CHAT_ID")
-    KEYWORDS = [k.strip().lower() for k in os.getenv("KEYWORDS", "").split(",")]
-    CHANNELS = [c.strip().lower() for c in os.getenv("CHANNELS", "").split(",")]
+    def __init__(self):
+        self.API_ID = int(os.getenv("API_ID", 0))
+        self.API_HASH = os.getenv("API_HASH", "")
+        self.BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+        self.CHAT_ID = os.getenv("CHAT_ID", "")
+        self.KEYWORDS = str_list(os.getenv("KEYWORDS", "").lower())
+        self.CHANNELS = str_list(os.getenv("CHANNELS", "").lower())
+
+        self.validate()
+
+    def validate(self):
+        if not self.API_ID or not self.API_HASH:
+            raise ValueError("API_ID and API_HASH must be set")
 
 settings = Settings()
